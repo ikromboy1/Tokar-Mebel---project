4ae9676f4bb899d31f285eea254c6bd2a90f96dd
ikromboy1 <ikromboykomiljanov761@gmail.com>
2025-05-20 15:00:51 +0500

    nav correct and react

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import React from "react";
 
 const App = () => {
   return (
     <div className="App">
       <Router>
-        <Routes>
           <Navbar />
+        <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            kfhd
+        </div>
+    )
+}
+
+export default Navbar
